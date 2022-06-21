cd266f17afb57129630a6f3ef6951bc05ce6385e
Daphne Lopez <daplopez@fb.com>
2022-06-21 15:13:55 -0700

    Implemented user sign-in using OAuth and user logout

---
+++ twitter/Models/User.m
@@ -0,0 +1,29 @@
+//
+//  User.m
+//  twitter
+//
+//  Created by Daphne Lopez on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+// Initializer
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+
+    if (self) {
+        self.name = dictionary[@"name"];
+        self.screenName = dictionary[@"screen_name"];
+        self.profilePicture = dictionary[@"profile_image_url_https"];
+        
+    // Initialize any other properties
+    }
+    return self;
+}
+
+
+
+@end
